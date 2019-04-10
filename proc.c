c25f76fddccaba630b463e04335e0b12e1022ef6
jafemm <38162959+jafemm@users.noreply.github.com>
2019-04-10 16:03:16 -0400

    Update proc.c

--- proc.c
+++ proc.c
@@ -539,14 +539,11 @@ int getprocs(void){
   struct proc *p;
   
   for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){    
-    printf("entre al for");
     if(p->state == EMBRYO || p->state == SLEEPING || p->state == RUNNABLE || p->state == RUNNING){
-      printf("entre al if");
       contador = contador+1;
       return contador;
     }
     else{
-      printf("entre al else");
       continue;
       return contador;    
     }
