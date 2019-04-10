730f37f3e8e057d0c70fc229b6988e87f8e8bc57
jafemm <38162959+jafemm@users.noreply.github.com>
2019-04-10 16:06:07 -0400

    Update proc.c

--- proc.c
+++ proc.c
@@ -537,15 +537,15 @@ int getprocs(void){
   
   int contador=0;
   struct proc *p;
-  
+  acquire(&ptable.lock);
   for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){    
     if(p->state == EMBRYO || p->state == SLEEPING || p->state == RUNNABLE || p->state == RUNNING){
       contador = contador+1;
-      return contador;
     }
     else{
-      continue;
-      return contador;    
+      continue;   
     }
   }
+  release(&ptable.lock);
+  return contador
 }
