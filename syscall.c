d3409fa1b586159f466564d3b24d163a2eb76232
Javiera Meneses <jmeneses@alumnos.uai.cl>
2019-04-10 17:01:55 -0400

    Pueba 7

--- syscall.c
+++ syscall.c
@@ -128,7 +128,7 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
-[SYS_close]   sys_getprocs,
+[SYS_getprocs]   sys_getprocs,
 };
 
 void
