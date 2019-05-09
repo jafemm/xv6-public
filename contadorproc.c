dacc7cdda161b52e92d458b94efd948f924e729f
Javiera Meneses <jmeneses@alumnos.uai.cl>
2019-05-08 21:23:12 -0400

    Travisx6

--- contadorproc.c
+++ contadorproc.c
@@ -1,18 +1,9 @@
-#include <stdio.h>
 #include "types.h"
-#include "defs.h"
-#include "param.h"
-#include "memlayout.h"
-#include "mmu.h"
-#include "x86.h"
-#include "proc.h"
-#include "spinlock.h"
 #include "stat.h"
+#include "user.h"
 
 
 int main(void){
-    printf("hola");
     printf(1,"El numero de procesos es: %d", getprocs());
-    printf("hola2");
     exit();
 }
