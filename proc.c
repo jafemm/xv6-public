ca99c8f03484465eab3a77e57f4a20a7e49d5e30
Javiera Meneses <jmeneses@alumnos.uai.cl>
2019-06-12 23:43:37 -0400

    Segunda Tarea P5

--- proc.c
+++ proc.c
@@ -7,7 +7,7 @@
 #include "proc.h"
 #include "spinlock.h"
 #define NULL 0
-#include "rand.h" 
+#include "rand.h"
 
 struct {
   struct spinlock lock;
@@ -321,12 +321,20 @@ wait(void)
 //  - swtch to start running that process
 //  - eventually that process transfers control
 //      via swtch back to the scheduler.
+
+
+
 void
 scheduler(void)
 {
   struct proc *p;
+  struct proc *current;
   struct cpu *c = mycpu();
   c->proc = 0;
+  int number_tickets = 100;               //Numero total de number_tickets
+  int counter = 0;
+  int total = 0;
+  int winner = 0;
 
   for(;;){
     // Enable interrupts on this processor.
@@ -334,9 +342,25 @@ scheduler(void)
 
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
+
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state==RUNNABLE){
+        total= total + p->tickets;
+      }
+    }
+    winner = random_at_most(total);
+
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
       if(p->state != RUNNABLE)
         continue;
+    current = p;
+    while (current!=NULL) {
+      counter = counter + current->tickets;
+      if(counter>winner){
+        break;
+      }
+      current = current->next;
+    }
 
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
@@ -547,52 +571,4 @@ int getprocs(void){
   }
   release(&ptable.lock);
   return contador;
-}
-
-void
-scheduler(void)
-{
-  struct proc *p;
-  struct cpu *c = mycpu();
-  struct proc *current;
-  c->proc = 0;
-  int number_tickets = 100;               //Numero total de number_tickets
-  int counter = 0;
-  srand(time(null));
-  int winner = rand()%(number_tickets);
-
-  for(;;){
-    // Enable interrupts on this processor.
-    sti();
-
-    // Loop over process table looking for process to run.
-    acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
-      current = p;
-      while (current!=NULL) {
-        counter = counter + current->tickets;
-        if(counter>winner)
-          break;
-        current = current->next;
-      }
-
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
-
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
-
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
-    }
-    release(&ptable.lock);
-
-  }
 }
